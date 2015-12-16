d1ae34c848e732e688fc812d4518339b494ef369
conomia <conomia_alfredo@hotmail.com>
2015-12-16 12:49:54 -0500

    corrección de la bd local

--- src/resources/DBManager.lua
+++ src/resources/DBManager.lua
@@ -102,17 +102,40 @@ local dbManager = {}
     dbManager.updateUser = function(idApp, email, password, name, apellido, condominio)
 		openConnection( )
         local query = "UPDATE config SET idApp = "..idApp..", email = '"..email.."', name = '"..name.."', apellido = '"..apellido.."', condominioId = '"..condominio.."';"
-        db:exec( query )
+        local result = db:exec( query )
+		--verifica si el update fallo
+		if result ~= 0 then
+			query = "DROP TABLE IF EXISTS config;"
+			db:exec( query )
+			query = "CREATE TABLE IF NOT EXISTS config (id INTEGER PRIMARY KEY, idApp INTEGER, email TEXT, password TEXT, name TEXT, apellido TEXT, "..
+					" condominioId INTEGER, url TEXT );"
+			db:exec( query )
+			query = "INSERT INTO config VALUES (1, 0, '', '', '', '', 0, 'http://geekbucket.com.mx/booking/');"
+			db:exec( query )
+			local query = "UPDATE config SET idApp = "..idApp..", email = '"..email.."', name = '"..name.."', apellido = '"..apellido.."', condominioId = '"..condominio.."';"
+			db:exec( query )
+		end
 		closeConnection( )
 	end
 	
 	--inserta los datos del condominio
 	dbManager.insertCondominium = function(items)
 		openConnection( )
+			query = "delete from condominios;"
+			db:exec( query )
 			for i = 1, #items, 1 do
 				local query = "INSERT INTO condominios VALUES ('" .. items[i].condominioId .."', '" .. items[i].id .."', '" .. items[i].nameCondominious .."');"
-				db:exec( query )
+				local result = db:exec( query )
+				if result ~= 0 then
+					query = "DROP TABLE IF EXISTS config;"
+					db:exec( query )
+					query = "CREATE TABLE IF NOT EXISTS condominios (id INTEGER, idUser INTEGER, nombre TEXT );"
+					db:exec( query )
+					query = "INSERT INTO condominios VALUES ('" .. items[i].condominioId .."', '" .. items[i].id .."', '" .. items[i].nameCondominious .."');"
+					db:exec( query )
+				end
 			end
+			
 		closeConnection( )
 	end
 	
@@ -127,9 +150,19 @@ local dbManager = {}
 	--inserta los datos del condominio
 	dbManager.insertResidencial = function(items)
 		openConnection( )
+			query = "delete from residencial;"
+			db:exec( query )
 			for i = 1, #items, 1 do
 				local query = "INSERT INTO residencial VALUES ('" .. items[i].id .."', '" .. items[i].nombre .."', '" .. items[i].telAdministracion .."', '" .. items[i].telCaseta .."', '" .. items[i].telLobby .."');"
-				db:exec( query )
+				local result = db:exec( query )
+				if result ~= 0 then
+					query = "DROP TABLE IF EXISTS residencial;"
+					db:exec( query )
+					query = "CREATE TABLE IF NOT EXISTS residencial (id INTEGER, nombre TEXT, telAdministracion TEXT, telCaseta TEXT, telLobby TEXT);"
+					db:exec( query )
+					local query = "INSERT INTO residencial VALUES ('" .. items[i].id .."', '" .. items[i].nombre .."', '" .. items[i].telAdministracion .."', '" .. items[i].telCaseta .."', '" .. items[i].telLobby .."');"
+					db:exec( query )
+				end
 			end
 		closeConnection( )
 	end
@@ -137,7 +170,7 @@ local dbManager = {}
 	--limpia la tabla de admin, guardia y condominio
     dbManager.clearUser = function()
         openConnection( )
-        query = "UPDATE config SET idApp = 0, email = '', password = '', name = '', apellido = '', condominioId = 0;"
+        query = "UPDATE config SET idApp = 0, email = '', password = '', name = '', apellido = '', condominioIdk = 0;"
         db:exec( query )
 		query = "delete from condominios;"
         db:exec( query )
